6ebca0da63871882ef7d4fa1bafa0e56c22ea44d
AmyHruska <amyhruskacda@gmail.com>
2023-10-26 10:14:35 -0700

    complete practice

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,28 @@
-function saySomething(whatToSay) {
-    window.alert(whatToSay);
-  }
-  
-  function add(number1, number2) {
+// business logic
+function add(number1, number2) {
     return number1 + number2;
-  }
-const result = add(3,5);
-window.alert(result);
+}
+function subtract(number1, number2) {
+    return number1 - number2;
+}
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+function divide(number1, number2) {
+    return number1 / number2;
+}
+
+
+// user interface logic 
+const number1 = parseInt(prompt("Please enter a number:"));
+const number2 = parseInt(prompt("Please enter another number:"));
+
+const addResult = add(number1, number2);
+
+const subtractResult = subtract(number1, number2);
+
+const multiplyResult = multiply(number1, number2);
+
+const divideResult = divide(number1, number2);
+
+window.alert(number1 + " + " + number2 + " = " + addResult + ", " + number1 + " - " + number2 + " = " + subtractResult + ", "  + number1 + " * " + number2 + " = " + multiplyResult + ", "  + number1 + " / " + number2 + " = " + divideResult + "." );
