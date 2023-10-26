af6fa04c9d80e68a3a913272a09c42d52f4d94a2
AmyHruska <amyhruskacda@gmail.com>
2023-10-26 09:11:39 -0700

    first real commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function saySomething(whatToSay) {
+    window.alert(whatToSay);
+  }
+  
+  function add(number1, number2) {
+    return number1 + number2;
+  }
+const result = add(3,5);
+window.alert(result);
